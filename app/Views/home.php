1a6a1b006f9f5c2a865cf4bfccd9ac02f64441fb
Salahudin-cloud <ayubi54321@gmail.com>
2025-01-25 20:59:53 +0700

    first commit

---
+++ app/Views/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Ini home page</p>
+</body>
+</html>
+
+
+<!-- tugas -->
+ <!-- buat view dan controller  :
+  1. Contact 
+  2. About
+  3. Services
+ -->
